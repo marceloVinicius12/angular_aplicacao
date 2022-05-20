e5f91b55078145458e9fcd779091919bd27fe8ce
marcelo vinicius <viniciusmarcello12@gmail.com>
2022-05-20 20:58:04 -0300

    adicionando componentes de professores e alunos

--- mv_school/src/app/app.module.ts
+++ mv_school/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { AlunosComponent } from './alunos/alunos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ProfessoresComponent,
+      AlunosComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
