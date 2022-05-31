61d06afb8fdb3ea2772471cfbe2d2a14bc3d1134
marcelo vinicius <viniciusmarcello12@gmail.com>
2022-05-30 22:33:03 -0300

    iniciando conexao npran

---
+++ Mv_school-API/Data/DataContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+namespace Mv_school_API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
--- Mv_school-API/models/AlunoDisciplina.cs
+++ Mv_school-API/models/AlunoDisciplina.cs
@@ -2,6 +2,20 @@ namespace Mv_school_API.models
 {
     public class AlunoDisciplina
     {
-        
+        public AlunoDisciplina()
+        {
+
+        }
+        public AlunoDisciplina(int alunoId, Aluno aluno, int disciplinaId )
+        {
+            this.AlunoId = alunoId;   
+            this.DisciplinaId = disciplinaId;
+
+              }
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+        public int DisciplinaId { get; set; }
+        public disciplina Disciplina { get; set; }
+
     }
 }
